44e651216ad169122387b489ff4370e9338e7acf
nikkei314 <nikkeijaneespina@gmail.com>
2025-12-26 17:01:36 +0800

    Update numpy_sample.py

--- Numpy/numpy_sample.py
+++ Numpy/numpy_sample.py
@@ -1,5 +1,9 @@
 import numpy as np
 
-arr = np.array([[1,2,3,4,5],[6,7,8,9,10]])
+arr1 = np.array([1,2,3])
 
-print(arr.shape)
+arr2 = np.array([4,5,6])
+
+arr = np.dstack((arr1, arr2))
+
+print(arr)
